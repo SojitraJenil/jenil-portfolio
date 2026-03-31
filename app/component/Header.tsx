b2448be645ecf9d348aa40ab66e9f47a67c68c8c
SojitraJenil <jenilsojitra4@gmail.com>
2026-03-31 14:02:25 +0530

    fix

--- app/component/Header.tsx
+++ app/component/Header.tsx
@@ -30,7 +30,6 @@ export default function Header() {
                 download
                 className="relative flex items-center gap-2 px-5 py-2 rounded-full 
                 bg-[#702cf6] text-white text-sm font-medium
-                animate-pulse hover:animate-none
                 hover:scale-105 active:scale-95 transition-all"
               >
                 <HiDownload size={16} />
